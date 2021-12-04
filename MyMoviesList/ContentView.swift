35ea5088b6e1a4d6d0630931341cdfde47b16d13
Arnaud <arnaud.nommay@orange.fr>
2021-12-04 22:46:21 +0100

    Initial Commit

---
+++ MyMoviesList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyMoviesList
+//
+//  Created by Arnaud on 04/12/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyMoviesList/MyMoviesListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesListApp.swift
+//  MyMoviesList
+//
+//  Created by Arnaud on 04/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
